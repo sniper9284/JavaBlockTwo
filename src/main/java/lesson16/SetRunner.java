a71f34f2b139eafca1ceb3c08a88727a1b78b1a2
sniper9284 <sniper-sochi@yandex.ru>
2021-06-28 10:30:36 +0300

    lesson 16 исправления по замечаниям.

--- src/main/java/lesson16/SetRunner.java
+++ src/main/java/lesson16/SetRunner.java
@@ -1,7 +1,6 @@
 package lesson16;
 
 import lesson16.setWork.Employee;
-
 import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.Random;
@@ -13,7 +12,7 @@ public class SetRunner {
         Set<Employee> employeeSet = new TreeSet<>(new Comparator<Employee>() {
             @Override
             public int compare(Employee o1, Employee o2) {
-                return o1.getWorkAge().compareTo(o2.getWorkAge()) * -1;
+               return o1.getWorkAge().compareTo(o2.getWorkAge()) * -1;
             }
         });
         employeeSet.add(new Employee("Ivanov", new BigDecimal(1)));
@@ -27,7 +26,7 @@ public class SetRunner {
         employeeSet.add(new Employee("Diachenko", new BigDecimal(9)));
         employeeSet.add(new Employee("Girenko", new BigDecimal(10)));
 
-        System.out.println("Вывод по убыванию стажа: " + employeeSet); // вывод по стажу
+        System.out.println("Вывод по убыванию стажа: " + employeeSet); //прямой вывод по стажу
 
         Random random = new Random(100);
         Set<Integer> intSet = new TreeSet<>();
@@ -36,16 +35,14 @@ public class SetRunner {
         }
 
         Set<Integer> intSet2 = new TreeSet<>();
-        for (int i = 0; i < 10; i++) {
-            intSet2.add(random.nextInt(100));
+        for (int i = 0; i < 100; i++) {
+            intSet2.add(random.nextInt(150));
         }
-
         print("Объединение множеств: ", union(intSet, intSet2));
         print("Пересечения множеств: ", intersact(intSet, intSet2));
     }
-
     // вывод на экран
-    private static void print(String string, Set<Integer> set) {
+    private static void print(String string, Set <Integer> set) {
         if (set.size() != 0) {
             System.out.print(string + " - ");
             for (Integer currentElement : set) {
@@ -55,14 +52,17 @@ public class SetRunner {
         }
     }
 
-    private static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
-        set1.addAll(set2);
-        return set1;
+    private static Set <Integer> union(Set <Integer> set1, Set <Integer> set2) {
+        Set <Integer> unionSet = new TreeSet <> ();
+        unionSet.addAll(set1);
+        unionSet.addAll(set2);
+        return unionSet;
     }
 
-    private static Set<Integer> intersact(Set<Integer> set1, Set<Integer> set2) {
-        set1.retainAll(set2);
-
-        return set1;
+    private static Set <Integer> intersact(Set <Integer> set1, Set <Integer> set2) {
+        Set <Integer> intersactSet = new TreeSet <> ();
+        intersactSet.addAll(set1);
+        intersactSet.retainAll(set2);
+        return intersactSet;
     }
 }
