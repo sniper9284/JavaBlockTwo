7a5229d8fadd73704c660ce470960b7e39f4616d
sniper9284 <sniper-sochi@yandex.ru>
2021-07-05 11:39:10 +0300

    lesson 17. застрял (((

--- src/main/java/lesson17/PhoneBookRunner.java
+++ src/main/java/lesson17/PhoneBookRunner.java
@@ -10,78 +10,70 @@ import java.util.*;
 public class PhoneBookRunner {
 
     public static void main(String[] args) {
-        LinkedList<Abonent> abonentlist = new LinkedList<>();
-        List<PhoneBook> phoneBookResult = new ArrayList<>();
-
+        LinkedList<PhoneBook> list = new LinkedList<>();
+        TreeSet<PhoneBook> set = new TreeSet<>((o1, o2) -> 0);
+        Map<Abonent, Collection<Abonent>> map = new HashMap<>();
         for (int i = 0; i < 1000; i++) {
-            Abonent abonent = new Abonent();
-            createAbonent(abonent);
-            abonentlist.add(abonent);
+            PhoneBook phoneBooksi = new PhoneBook(createOwnerAbonent(new Abonent()), createContacts(new ArrayList<>()));
+            list.add(phoneBooksi);
+            set.add(phoneBooksi);
+            map.put(phoneBooksi.getAbonent(), phoneBooksi.getContacts());
         }
-        for (int i = 0; i < 1000; i++) {
-            PhoneBook phone = new PhoneBook();
-            phone.setAbonent(abonentlist.get(i));
-            phone.setContacts(createContacts(abonentlist));
-            phoneBookResult.add(phone);
-        }
-        findToSet(phoneBookResult);
-        findToList(phoneBookResult);
+        findToList(list);
+        findToSet(set);
+        findToMap(map);
     }
 
-    private static void findToList(List<PhoneBook> result) {
+    private static void findToSet(TreeSet treeSet) {
         Instant start = Instant.now();
-        LinkedList<Collection> commonContacts = new LinkedList<>();
-        for (int k1 = 0; k1 < result.size(); k1++) {
-            PhoneBook p1 = result.get(k1);
-            for (int k2 = k1 + 1; k2 < result.size() - k1; k2++) {
-                PhoneBook p2 = result.get(k2);
-                commonContacts.addAll(intersect(p1, p2));
-            }
-        }
-        System.out.println("--------------");
-        System.out.println(commonContacts);
+        List<PhoneBook> phoneBookList = new ArrayList<>();
+        phoneBookList.add((PhoneBook) treeSet.descendingIterator().next());
+        phoneBookList.retainAll(treeSet);
         Instant finish = Instant.now();
         long elapsed = Duration.between(start, finish).toMillis();
+        System.out.println(phoneBookList);
         System.out.println("Прошло времени, мс: " + elapsed);
     }
 
-    private static void findToSet(List<PhoneBook> result) {
+    private static void findToMap(Map map) {
+        Iterator<Map.Entry<Abonent, Collection<Abonent>>> entryIterator =
+                new TreeSet<>(map.entrySet()).descendingIterator();
+            //понять как быть дальше
+    }
+
+    private static void findToList(LinkedList linkedList) {
         Instant start = Instant.now();
-        Set<Collection> commonContacts = new HashSet<>();
-        for (int k1 = 0; k1 < result.size(); k1++) {
-            PhoneBook p1 = result.get(k1);
-            for (int k2 = k1 + 1; k2 < result.size() - k1; k2++) {
-                PhoneBook p2 = result.get(k2);
-                commonContacts.addAll(intersect(p1, p2));
+        List<PhoneBook> resultList = new ArrayList<>();
+        Iterator<PhoneBook> phonebookIterator = linkedList.descendingIterator();
+        while (phonebookIterator.hasNext()) {
+            PhoneBook next = phonebookIterator.next();
+            Iterator<PhoneBook> phonebookIterator2 = linkedList.descendingIterator();
+            while (phonebookIterator2.hasNext()) {
+                PhoneBook next2 = phonebookIterator2.next();
+                if (next.equals(next2)) {
+                    resultList.add(next2);
+                }
             }
         }
-        System.out.println("--------------");
-        System.out.println(commonContacts);
         Instant finish = Instant.now();
         long elapsed = Duration.between(start, finish).toMillis();
+        System.out.println(resultList);
         System.out.println("Прошло времени, мс: " + elapsed);
     }
 
-    private static Abonent createAbonent(Abonent abonent) {
-        abonent.createFio();
-        abonent.createNumber();
-        return abonent;
+    private static Abonent createOwnerAbonent(Abonent ownerAbonent) {
+        ownerAbonent.createFio();
+        ownerAbonent.createNumber();
+        return ownerAbonent;
     }
 
-    private static Collection<Abonent> createContacts(LinkedList<Abonent> bookPhone) {
-        Random random = new Random();
-        LinkedList<Abonent> contacts = new LinkedList<>();
-        for (int j = 0; j < 100; j++) {
-            contacts.add(bookPhone.get(random.nextInt(1000)));
+    private static Collection<Abonent> createContacts(Collection<Abonent> bookPhone) {
+        for (int j = 0; j < 10; j++) {
+            Abonent aj = new Abonent();
+            aj.createFio();
+            aj.createNumber();
+            bookPhone.add(aj);
         }
-        return contacts;
-    }
-    private static List<Collection> intersect(PhoneBook p1, PhoneBook p2) {
-        List<Collection> phoneBooksP1 = new ArrayList<>();
-        phoneBooksP1.add(p1.getContacts());
-        List<Collection> phoneBooksP2 = new ArrayList<>();
-        phoneBooksP2.add(p2.getContacts());
-        phoneBooksP1.removeAll(phoneBooksP2);
-        return phoneBooksP1;
+        return bookPhone;
     }
 }
--- src/main/java/lesson17/book/PhoneBook.java
+++ src/main/java/lesson17/book/PhoneBook.java
@@ -1,12 +1,9 @@
 package lesson17.book;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Collection;
 
 @Data
-@NoArgsConstructor
 public class PhoneBook {
     private Abonent abonent;
     private Collection<Abonent> contacts;
