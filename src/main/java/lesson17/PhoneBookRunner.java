119d087e523da011e3929d0490d99d92de711027
sniper9284 <sniper-sochi@yandex.ru>
2021-07-05 18:37:53 +0300

    lesson17 1.2

--- src/main/java/lesson17/PhoneBookRunner.java
+++ src/main/java/lesson17/PhoneBookRunner.java
@@ -3,8 +3,6 @@ package lesson17;
 import lesson17.book.Abonent;
 import lesson17.book.PhoneBook;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.*;
 
 public class PhoneBookRunner {
@@ -19,61 +17,8 @@ public class PhoneBookRunner {
             set.add(phoneBooksi);
             map.put(phoneBooksi.getAbonent(), phoneBooksi.getContacts());
         }
-        findToList(list);
-        findToSet(set);
-        findToMap(map);
     }
 
-    private static void findToSet(TreeSet treeSet) {
-        Instant start = Instant.now();
-        TreeSet<PhoneBook> result = new TreeSet<>((o1, o2) -> 0);
-        Iterator<PhoneBook> phoneBookIterator = treeSet.descendingIterator();
-        while (phoneBookIterator.hasNext()) {
-            PhoneBook next = phoneBookIterator.next();
-            Iterator<PhoneBook> phonebookIterator2 = treeSet.descendingIterator();
-            while (phonebookIterator2.hasNext()) {
-                PhoneBook next2 = phonebookIterator2.next();
-                if (next.equals(next2)) {
-                    result.add(next2);
-                }
-            }
-        }
-        Instant finish = Instant.now();
-        long elapsed = Duration.between(start, finish).toMillis();
-        System.out.println(result);
-        System.out.println("Прошло времени, мс: " + elapsed);
-    }
-
-    private static void findToMap(Map map) {
-        Instant start = Instant.now();
-        Iterator<Map.Entry<Abonent, Collection<Abonent>>> entryIterator =
-                new TreeSet<>(map.entrySet()).descendingIterator();
-        //...
-        Instant finish = Instant.now();
-        long elapsed = Duration.between(start, finish).toMillis();
-        System.out.println( );
-        System.out.println("Прошло времени, мс: " + elapsed);
-    }
-
-    private static void findToList(LinkedList linkedList) {
-        Instant start = Instant.now();
-        LinkedList<PhoneBook> resultList = new LinkedList<>();
-        Iterator<PhoneBook> phonebookIterator = linkedList.descendingIterator();
-        while (phonebookIterator.hasNext()) {
-            PhoneBook next = phonebookIterator.next();
-            Iterator<PhoneBook> phonebookIterator2 = linkedList.descendingIterator();
-            while (phonebookIterator2.hasNext()) {
-                PhoneBook next2 = phonebookIterator2.next();
-                if (next.equals(next2)) {
-                    resultList.add(next2);
-                }
-            }
-        }
-        Instant finish = Instant.now();
-        long elapsed = Duration.between(start, finish).toMillis();
-        System.out.println(resultList);
-        System.out.println("Прошло времени, мс: " + elapsed);
-    }
 
     private static Abonent createOwnerAbonent(Abonent ownerAbonent) {
         ownerAbonent.createFio();
